c2d401f7a44f467a38f7314c71561e5207b048c1
Abraham <nencho_1@hotmail.com>
2021-01-10 16:25:57 +0100

    Finished all function but there is problem on the function buyCookies

--- assets/JS/myFunction.js
+++ assets/JS/myFunction.js
@@ -6,19 +6,38 @@ const cookiesProduction = [ 1, 2, 4];
 const cookiesPrice = [ 100, 200, 400];
 
 const click = () => {
-    document.querySelector('#cookie-image').onclick = cookies++; 
+    cookies++; 
 };
 
-//const buyMoreCookies = b => {
-//    document.querySelector('#btn-cursor').onclick = 0;
-//};
+const buyMoreCookies = Object => {
+    if( cookies >= cookiesPrice[Object] ) {
+        cookiesAmounth[Object]++;
+    } 
+    
+    return 'You don´t have enough money';
+};
+
+const createCookies = () => {
+    for( let i = 0; i < cookiesAmounth.length; i++) {
+        cookies += cookiesAmounth[i] * cookiesProduction[i];
+    }
+};
 
 const render = () => {
     document.querySelector('#count').innerHTML = cookies;
+    document.querySelector('#inventory').innerHTML = 
+        `Cursor: ${cookiesAmounth[0]}<br>
+         Abuelita: ${cookiesAmounth[1]}<br>
+         Horno: ${cookiesAmounth[2]}`
 };
 
+document.querySelector('#cookie-image').onclick = click;
+document.querySelector('#btn-cursor').onclick = buyMoreCookies[0];
+document.querySelector('#btn-granma').onclick = buyMoreCookies[1];
+document.querySelector('#btn-oven').onclick = buyMoreCookies[2];
+
 //PARA RENDERIZAR VISTAS A LA VELOCIDAD QUE LE DIGAMOS
-const FPS = 30;
+const FPS = 10;
 
 setInterval(() => {
     render();
